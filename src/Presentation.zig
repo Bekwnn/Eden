5a8759f6f7d6b83a36d105c050dbfce871bb7f3d
Bekwnn <bekwnn@gmail.com>
2020-02-01 14:36:02 -0800

    refactored shader loading to load external files and return a shader object

--- src/Presentation.zig
+++ src/Presentation.zig
@@ -1,107 +1,17 @@
 const GameWorld = @import("GameWorld.zig").GameWorld;
+const Shader = @import("Shader.zig").Shader;
 const debug = @import("std").debug;
 
 usingnamespace @import("c.zig");
 
-var curShader: ?GLuint = null;
+var curShader: ?Shader = null;
 
 pub fn Initialize(renderer: *SDL_Renderer) void {
-    glDepthFunc(GL_LESS);
     glClearColor(0.1, 0.1, 0.2, 1.0);
-    curShader = LoadShader("basivert", "basifrag");
+    curShader = Shader.init(
+    "src\\shaders\\basic.vert", 
+    "src\\shaders\\basic.frag");
     BindVAO();
-}
-
-//vertex shader: build, compile, link
-// TODO should live in its own file
-pub fn LoadShader(vertFile: []const u8, fragFile: []const u8) ?GLuint {
-    var compileResult: GLint = GL_FALSE;
-
-    // convert and compile
-    debug.warn("Compiling {}...\n", vertFile);
-    const vertProgramText = ReadVShader(vertFile); // needs to be GLchar*
-    const vertShaderObject: GLuint = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertShaderObject, 1, &vertProgramText, 0);
-    glCompileShader(vertShaderObject);
-
-    glGetShaderiv(vertShaderObject, GL_COMPILE_STATUS, &compileResult);
-    if (compileResult == GL_FALSE) {
-        var errLogLength: GLint = 0;
-        glGetShaderiv(vertShaderObject, GL_INFO_LOG_LENGTH, &errLogLength);
-        var errLog: []u8 = undefined;
-        glGetShaderInfoLog(vertShaderObject, errLogLength, &errLogLength, &errLog[0]);
-        debug.warn("{}\n", errLog[0..@intCast(usize, errLogLength)]);
-        return null;
-    } else {
-        debug.warn("Vertex shader {} compiled successfully.\n", vertFile);
-    }
-
-    // fragment shader: build, compile, link
-    debug.warn("Compiling {}...\n", fragFile);
-    const fragProgramText = ReadFShader(fragFile); // needs to be GLchar*
-    const fragShaderObject: GLuint = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragShaderObject, 1, &fragProgramText, 0);
-    glCompileShader(fragShaderObject);
-
-    glGetShaderiv(fragShaderObject, GL_COMPILE_STATUS, &compileResult);
-    if (compileResult == GL_FALSE) {
-        var errLogLength: GLint = 0;
-        glGetShaderiv(fragShaderObject, GL_INFO_LOG_LENGTH, &errLogLength);
-        var errLog: []u8 = undefined;
-        glGetShaderInfoLog(fragShaderObject, errLogLength, &errLogLength, &errLog[0]);
-        debug.warn("{}\n", errLog[0..@intCast(usize, errLogLength)]);
-        return null;
-    } else {
-        debug.warn("Fragment shader {} compiled successfully.\n", fragFile);
-    }
-
-    // link shaders
-    debug.warn("Linking shader programs {} and {}...\n", vertFile, fragFile);
-    const shaderObject: GLuint = glCreateProgram();
-    glAttachShader(shaderObject, vertShaderObject);
-    glAttachShader(shaderObject, fragShaderObject);
-    glLinkProgram(shaderObject);
-
-    glGetProgramiv(shaderObject, GL_LINK_STATUS, &compileResult);
-    if (compileResult == GL_FALSE) {
-        var errLogLength: GLint = 0;
-        glGetProgramiv(shaderObject, GL_INFO_LOG_LENGTH, &errLogLength);
-        var errLog: []u8 = undefined;
-        glGetProgramInfoLog(shaderObject, errLogLength, &errLogLength, &errLog[0]);
-        debug.warn("{}\n", errLog[0..@intCast(usize, errLogLength)]);
-        return null;
-    }
-    debug.warn("Shader program {} and {} linked successfully.\n", vertFile, fragFile);
-
-    // compiled shaders are linked to program, cleanup/delete source
-    glDeleteShader(vertShaderObject);
-    glDeleteShader(fragShaderObject);
-
-    return shaderObject;
-}
-
-//TODO read from file
-fn ReadVShader(shaderFileName: []const u8) [*]const u8 {
-    return
-        c\\#version 330 core
-        c\\layout (location = 0) in vec3 aPos;
-        c\\
-        c\\void main()
-        c\\{
-        c\\    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-        c\\}
-    ;
-}
-fn ReadFShader(shaderFileName: []const u8) [*]const u8 {
-    return
-        c\\#version 330 core
-        c\\out vec4 FragColor;
-        c\\
-        c\\void main()
-        c\\{
-        c\\    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-        c\\}
-    ;
 }
 
 const vertices = [_]f32{
@@ -143,7 +53,7 @@ pub fn RenderFrame(renderer: *SDL_Renderer, screen: *SDL_Window, gameWorld: *con
     glClear(GL_COLOR_BUFFER_BIT);
 
     if (curShader) |s| {
-        glUseProgram(s);
+        glUseProgram(s.gl_id);
     }
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, null);
---
+++ src/Shader.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+const debug = std.debug;
+const File = std.fs.File;
+//const path = std.fs.path;
+const Allocator = std.mem.Allocator;
+
+usingnamespace @import("c.zig");
+
+var allocator = std.heap.direct_allocator;
+
+const ShaderCompileErr = error {
+    SeeLog,
+};
+
+fn ShaderTypeStr(shaderType: GLenum) []const u8 {
+    return switch (shaderType){
+        GL_VERTEX_SHADER => "Vertex",
+        GL_FRAGMENT_SHADER => "Fragment",
+        GL_COMPUTE_SHADER => "Compute",
+        GL_GEOMETRY_SHADER => "Geometry",
+        GL_TESS_CONTROL_SHADER => "Tessalation Control",
+        GL_TESS_EVALUATION_SHADER => "Tessalation Evaluation",
+        else => return "Unknown",
+    };
+}
+
+// return needs to be cleaned up with glDeleteShader
+fn CompileShader(relativePath: []const u8, shaderType: GLenum) !GLuint {
+    var compileResult: GLint = GL_FALSE;
+
+    debug.warn("Compiling {} Shader {}...\n", ShaderTypeStr(shaderType), relativePath);
+
+    const cwd = try std.process.getCwdAlloc(allocator);
+    defer allocator.free(cwd);
+
+    var fullPath = try allocator.alloc(u8, cwd.len + 1 + relativePath.len);
+    defer allocator.free(fullPath);
+    std.mem.copy(u8, fullPath[0..], cwd);
+    std.mem.copy(u8, fullPath[cwd.len..], "\\");
+    std.mem.copy(u8, fullPath[cwd.len+1..], relativePath);
+
+    debug.warn("Full Path: {}\n", fullPath);
+
+    const shaderFile = try File.openRead(fullPath);
+    defer shaderFile.close();
+
+    var shaderCode = try allocator.alloc(u8, try shaderFile.getEndPos());
+    defer allocator.free(shaderCode);
+
+    const shaderObject: GLuint = glCreateShader(shaderType);
+    _ = try shaderFile.read(shaderCode);
+    const shaderSrcPtr: ?[*]const u8 = shaderCode.ptr;
+    glShaderSource(shaderObject, 1, &shaderSrcPtr, 0);
+    glCompileShader(shaderObject);
+
+    glGetShaderiv(shaderObject, GL_COMPILE_STATUS, &compileResult);
+    if (compileResult == GL_FALSE) {
+        var errLogLength: GLint = 0;
+        glGetShaderiv(shaderObject, GL_INFO_LOG_LENGTH, &errLogLength);
+        var errLog = try allocator.alloc(u8, @intCast(usize, errLogLength));
+        glGetShaderInfoLog(shaderObject, errLogLength, &errLogLength, errLog.ptr); //this line segfaults?
+        debug.warn("{}\n", errLog[0..@intCast(usize, errLogLength)]);
+        return ShaderCompileErr.SeeLog;
+    } else {
+        debug.warn("{} shader {} compiled successfully.\n", ShaderTypeStr(shaderType), relativePath);
+    }
+
+    return shaderObject;
+}
+
+pub const Shader = struct {
+    gl_id: GLuint,
+
+    pub fn init(vertFile: []const u8, fragFile: []const u8) ?Shader {
+
+        // vert shader: build, compile, link
+        const vertShaderObject = CompileShader(vertFile, GL_VERTEX_SHADER) catch |err| {
+            debug.warn("Unable to compile {} shader with path {}, error: {}\n", ShaderTypeStr(GL_VERTEX_SHADER), vertFile, err);
+            return null;
+        }; 
+        defer glDeleteShader(vertShaderObject);
+
+        // fragment shader: build, compile, link
+        // vert shader: build, compile, link
+        const fragShaderObject = CompileShader(fragFile, GL_FRAGMENT_SHADER) catch |err| {
+            debug.warn("Unable to compile {} shader with path {}, error: {}\n", ShaderTypeStr(GL_FRAGMENT_SHADER), fragFile, err);
+            return null;
+        }; 
+        defer glDeleteShader(fragShaderObject);
+
+        // link shaders
+        debug.warn("Linking shader programs {} and {}...\n", vertFile, fragFile);
+        const shaderObject: GLuint = glCreateProgram();
+        glAttachShader(shaderObject, vertShaderObject);
+        glAttachShader(shaderObject, fragShaderObject);
+        glLinkProgram(shaderObject);
+
+        var compileResult: GLint = GL_FALSE;
+        glGetProgramiv(shaderObject, GL_LINK_STATUS, &compileResult);
+        if (compileResult == GL_FALSE) {
+            var errLogLength: GLint = 0;
+            glGetProgramiv(shaderObject, GL_INFO_LOG_LENGTH, &errLogLength);
+            var errLog: []u8 = undefined;
+            glGetProgramInfoLog(shaderObject, errLogLength, &errLogLength, &errLog[0]);
+            debug.warn("{}\n", errLog[0..@intCast(usize, errLogLength)]);
+            return null;
+        }
+        debug.warn("Shader program {} and {} linked successfully.\n", vertFile, fragFile);
+
+        // compiled shaders are linked to program, cleanup/delete source
+        glDeleteShader(fragShaderObject);
+
+        return Shader{.gl_id = shaderObject};
+    }
+};
+

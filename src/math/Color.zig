bb0108f380b2b6528796daef75b831108cd8ed05
Bekwnn <bekwnn@gmail.com>
2020-03-23 20:38:10 -0700

    added mat3x3 and mat4x4 structs, added toHSV and fromHSV to color

--- src/math/Color.zig
+++ src/math/Color.zig
@@ -31,6 +31,70 @@ pub const ColorRGBA8 = struct {
     a: u8,
 };
 
+pub const ColorHSV = struct {
+    h: f32, // angle in degrees
+    s: f32, // 0 to 1
+    v: f32, // 0 to 1
+};
+
+// FromHSV
+//TODO HSV8 / RGB8 version
+pub inline fn HSVToRGB(color: *const ColorHSV) ColorRGB {
+    if (color.s == 0.0) { // grey-scale
+        return ColorRGB{ .r = color.v, .g = color.v, .b = color.v };
+    }
+
+    const hh = color.h / 60.0;
+    const i = @floatToInt(u8, hh);
+    const ff = hh - @intToFloat(f32, i);
+    const p = color.v * (1.0 - color.s);
+    const q = color.v * (1.0 - (color.s * ff));
+    const t = color.v * (1.0 - (color.s * (1.0 - ff)));
+
+    return switch (i) {
+        0 => ColorRGB{ .r = color.v, .g = t, .b = p },
+        1 => ColorRGB{ .r = q, .g = color.v, .b = p },
+        2 => ColorRGB{ .r = p, .g = color.v, .b = t },
+        3 => ColorRGB{ .r = p, .g = q, .b = color.v },
+        4 => ColorRGB{ .r = t, .g = p, .b = color.v },
+        else => ColorRGB{ .r = color.v, .g = p, .b = q },
+    };
+}
+
+// ToHSV
+//TODO HSV8 / RGB8 version
+pub inline fn RGBToHSV(color: *const ColorRGB) ColorHSV {
+    const min = zigm.min(zigm.min(color.r, color.g), color.b);
+    const max = zigm.max(zigm.max(color.r, color.g), color.b);
+    const delta = max - min;
+
+    var outHSV = ColorHSV{ .h = 0.0, .s = 0.0, .v = max };
+
+    if (delta < zigm.f32_epsilon) //grey-scale color
+        {
+        return outHSV;
+    } else {
+        outHSV.s = (delta / max);
+    }
+
+    if (color.r >= max) {
+        outHSV.h = (color.g - color.b) / delta; // between magenta & yellow
+    } else {
+        if (color.g >= max) {
+            outHSV.h = 2.0 + (color.b - color.r) / delta; // between cyan & yellow
+        } else {
+            outHSV.h = 4.0 + (color.r - color.g) / delta; // between magenta & cyan
+        }
+    }
+
+    outHSV.h *= 60.0; // degrees
+
+    if (outHSV.h < 0.0)
+        outHSV.h += 360.0;
+
+    return outHSV;
+}
+
 // ToRGB
 pub inline fn RGB8ToRGB(color: *const ColorRGB8) ColorRGB {
     return ColorRGB{ .r = color.r / 255.0, .g = color.g / 255.0, .b = color.b / 255 };
--- src/math/Math.zig
+++ src/math/Math.zig
@@ -5,3 +5,7 @@ pub const vec2 = @import("Vec2.zig");
 pub const Vec2 = vec2.Vec2;
 pub const quat = @import("Quat.zig");
 pub const Quat = quat.Quat;
+pub const mat3x3 = @import("Mat3x3.zig");
+pub const Mat3x3 = mat3x3.Mat3x3;
+pub const mat4x4 = @import("Mat4x4.zig");
+pub const Mat4x4 = mat4x4.Mat4x4;

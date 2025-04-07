cd86c0d1a99c414836f3fba2658e79c44c621c72
EvanN <e.w.nickerson@gmail.com>
2025-04-07 01:58:53 -0700

    rendering loop wip

--- src/presentation/RenderObject.zig
+++ src/presentation/RenderObject.zig
@@ -11,12 +11,48 @@ const Material = @import("Material.zig").Material;
 const Buffer = @import("Buffer.zig").Buffer;
 
 pub const RenderObject = struct {
-    m_indexCount: u32,
-    m_firstIndex: u32,
+    const Self = @This();
+
     m_indexBuffer: Buffer,
+    m_vertBuffer: Buffer,
 
     m_material: *Material,
+    m_mesh: *Mesh,
 
     m_transform: Mat4x4 = mat4x4.identity,
-    m_vertBufferAddress: c.VkDeviceAddress,
+
+    pub fn Draw(self: Self, cmd: c.VkCommandBuffer) !void {
+        //bind material descriptor sets
+
+        //bind vertex and index buffers
+        const offsets = [_]c.VkDeviceSize{0};
+        const vertexBuffers = [_]c.VkBuffer{
+            self.m_vertBuffer.m_buffer,
+        };
+        c.vkCmdBindVertexBuffers(
+            cmd,
+            0,
+            1,
+            &vertexBuffers,
+            &offsets,
+        );
+        c.vkCmdBindIndexBuffer(
+            cmd,
+            self.m_indexBuffer.m_buffer,
+            0,
+            c.VK_INDEX_TYPE_UINT32,
+        );
+
+        //bind per object descriptor sets
+        c.vkCmdBindDescriptorSets(
+            cmd,
+            c.VK_PIPELINE_BIND_POINT_GRAPHICS,
+            self.m_material.m_shaderPass.m_pipelineLayout,
+            0,
+            1,
+            self.m_material.m_shaderPass.m_shaderEffect.
+            0,
+            null,
+        );
+    }
 };
